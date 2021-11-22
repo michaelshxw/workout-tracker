36bfe22bba34a8f82b5a3e3d721a84211fadc83c
michaelshxw <michaelshxw@gmail.com>
2021-11-22 21:04:59 +0800

    begin api.js with comment code

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const db = require('../models');
+const router = require('express').Router();
+
+// get workouts
+
+
+// add exercise
+
+
+// create workout
+
+
+// get workouts by type
+
+
+module.exports = router; 
