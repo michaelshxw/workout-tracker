b719051e897f02c583fb36b44ea76048b66bb20e
michaelshxw <michaelshxw@gmail.com>
2021-11-23 18:49:31 +0800

    finish get all workouts api

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,22 @@ const router = require('express').Router();
 
 // get workouts
 
+router.get('/api/workouts', (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+        console.log('all workouts');
+        console.log(dbWorkout);
+        dbWorkout.forEach(workout => {
+            const total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+        })
+        res.json(dbWorkout);
+    }).catch(error => {
+        res.json(error);
+    });
+});
 
 // add exercise
 
