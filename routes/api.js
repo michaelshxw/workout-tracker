fff2872d29881a253d3f108c934ce1c03c38c3e9
michaelshxw <michaelshxw@gmail.com>
2021-11-23 19:11:45 +0800

    finsh all routes - final commit (hopefully)

--- routes/api.js
+++ routes/api.js
@@ -12,11 +12,14 @@ router.get('/api/workouts', (req, res) => {
                 total += e.duration;
             });
             workout.totalDuration = total;
-        })
+
+        });
+
         res.json(dbWorkout);
     }).catch(error => {
         res.json(error);
     });
+
 });
 
 // add exercise
@@ -25,7 +28,7 @@ router.put('/api/workouts/:id', (req, res) => {
 
     db.Workout.findOneAndUpdate(
         { _id: req.params.id },
-        { 
+        {
             $inc: { totalDuration: req.body.duration },
             $push: { exercises: req.body }
         },
@@ -34,12 +37,30 @@ router.put('/api/workouts/:id', (req, res) => {
         }).catch(error => {
             res.json(error);
         });
+
 });
 
 // create workout
 
+router.post('/api/workouts', ({ body }, res) => {
+
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(error => {
+        res.json(error);
+    });
+
+});
 
 // get workouts by type
 
+router.get('/api/workouts/type', (req, res) => {
+
+    db.Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(error => {
+        res.json(error)
+    })
+})
 
 module.exports = router;
